9df7d3f7dfacdb236c011b6e75c66cc261539672
SeymourTalletti <42839411+SeymourTalletti@users.noreply.github.com>
2018-08-31 19:45:07 +0400

    31,08,2018
    Программа для вычисления четности чисел.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите число :");
+                 a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0) 
+            {
+                Console.WriteLine("Число " + a + " - чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + a + " - нечётное");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
